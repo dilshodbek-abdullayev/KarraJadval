515bf9a5d8439c39d1ee1e81715a791ddde300e6
Dilshodbek Abdullayev <abdullayevdilshodbek1998@gmail.com>
2024-06-05 20:59:40 +0500

    finished

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+Console.Write("Karra jadval boshlanishi: ");
+int start = Convert.ToInt32(Console.ReadLine());
+Console.Write("Karra jadval tugashi: ");
+int end = Convert.ToInt32(Console.ReadLine());
+
+        // Ustun kengligini belgilash
+        int columnWidth = 15;
+
+        // Har bir ustun uchun natijalarni chiqarish
+        for (int j = 1; j <= 10; j++)
+        {
+            for (int i = start; i <= end; i++)
+            {
+                // Natijani chiqarish va ustun kengligiga moslash
+                string result = $"{i} * {j} = {i * j}";
+                Console.Write(result.PadRight(columnWidth));
+            }
+            Console.WriteLine(); 
+        }
--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# KarraJadval
+
+>
+> # Karra Jadval
+>
+## Unda ikkita for siklidan foydalanildi.Foydalanuvchi boshlang'ich va tugash qiymat kiritadi,ularni orasidagi qiymatlar console ga chiqariladi
+
+***Dastur codi***
+![alt text](image.png)
+
+***Dastur ishlashi***
+![alt text](image-1.png)
